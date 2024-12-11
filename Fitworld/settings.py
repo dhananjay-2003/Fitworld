a31c15e5d8945ba9a3628f7308a5892902864d06
dhananjay-2003 <dhananjaynalawade6210@gmail.com>
2024-12-11 12:47:37 +0530

    Update settings.py

--- Fitworld/settings.py
+++ Fitworld/settings.py
@@ -141,7 +141,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'nalawadedhananjaycode123@gmail.com'
-# EMAIL_HOST_PASSWORD = 'Dhananjay@20'
 EMAIL_HOST_PASSWORD = 'cgwj bdtw bmwx gvil'
 
 
